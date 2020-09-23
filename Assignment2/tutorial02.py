9ffe79329e64c24a7b6b1b3e25249df772e31f3a
Pritam Singh <pkspritam10@gmail.com>
2020-09-23 18:47:29 +0530

    added Assignment1 folder , implemented summation function
    Signed-off-by: Pritam Singh <pkspritam10@gmail.com>

---
+++ Assignment2/tutorial02.py
@@ -0,0 +1,8 @@
+# Function to compute sum. You cant use Python functions
+def summation(first_list):
+    # sum Logic
+    summation_value = 0
+    for item in first_list:
+        summation_value+=item
+    return summation_value
+
