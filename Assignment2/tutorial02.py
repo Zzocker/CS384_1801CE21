0365b9a17ab0a912595b6a973d23afbb358320be
Pritam Singh <pkspritam10@gmail.com>
2020-09-23 20:04:14 +0530

    implemented sorting functions ; takes list and returns sorted list
    Signed-off-by: Pritam Singh <pkspritam10@gmail.com>

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -6,3 +6,13 @@ def summation(first_list):
         summation_value+=item
     return summation_value
 
+def sorting(first_list):
+    sorted_list = first_list[:]
+    for i in range(1,len(sorted_list)):
+        plug = sorted_list[i]
+        j = i-1
+        while  j>=0 and sorted_list[j]>plug:
+            sorted_list[j+1]=sorted_list[j]
+            j-=1
+        sorted_list[j+1]=plug
+    return sorted_list
