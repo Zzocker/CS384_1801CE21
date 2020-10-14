90e5f52f6e7d49e3bb19b418c9154ea2b715f549
Pritam Singh <pkspritam10@gmail.com>
2020-10-14 22:32:45 +0530

    added course function; separates students based on course taken by them
    Signed-off-by: Pritam Singh <pkspritam10@gmail.com>

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -1,15 +1,48 @@
 import csv
-
+import re
+import os
 
 def course():
     # Read csv and process
-    pass
-
-
+    swticher = {
+        "01" : "btech",
+        "11" : "mtech",
+        "12" : "msc",
+        "21" : "phd"
+    }
+    base = "analytics/course"
+    with open("./studentinfo_cs384.csv","r") as infile:
+        pattern = re.compile("[0-9]{4}[a-zA-Z]{2}[0-9]{2}")
+        reader = csv.DictReader(infile)
+        fieldname = reader.fieldnames
+        misfile = open("{}/misc.csv".format(base),"w")
+        miswritter = csv.DictWriter(misfile,fieldname)
+        miswritter.writeheader()
+        next(reader,None)
+        for row in reader:
+            if re.match(pattern,row.get("id")):
+                roll_no =row.get("id")
+                year = roll_no[:2]
+                curs = swticher.get(roll_no[2:4])
+                branch = roll_no[4:6].lower()
+                foldername = "{}/{}/{}".format(base,branch,curs)
+                try:
+                    os.makedirs(foldername)
+                except:
+                    pass
+                filename = "{}/{}_{}_{}.csv".format(foldername,year,branch,curs)
+                if os.path.isfile(filename) == False:
+                    with open(filename,"w") as f:
+                        csv.DictWriter(f,fieldname).writeheader()
+                with open(filename,"a") as f:
+                    csv.DictWriter(f,fieldname).writerow(row)
+            else:
+                with open("{}/misc.csv".format(base),"a") as f:
+                    csv.DictWriter(f,fieldname).writerow(row)
+                    
 def country():
     # Read csv and process
     pass
-
 
 def email_domain_extract():
     # Read csv and process
