74cfbf7d9122ea44e08fda48ae302b0589c1e950
Pritam Singh <pkspritam10@gmail.com>
2020-11-10 22:46:43 +0530

    added clean function;for removing csv files from grades folder
    Signed-off-by: Pritam Singh <pkspritam10@gmail.com>

--- Assignment4/main.py
+++ Assignment4/main.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 def getreader():
     """
@@ -7,3 +8,15 @@ def getreader():
     infile= open("./acad_res_stud_grades.csv")
     reader = csv.DictReader(infile)
     return reader
+
+def clean():
+    """
+    removes 
+    1. all csv files from grades folder
+    2. misc.csv file
+    """
+    base = "./grades"
+    for f in os.listdir(base):
+        os.remove('{}/{}'.format(base,f))
+    if os.path.isfile('misc.csv'):
+        os.remove('./misc.csv')
