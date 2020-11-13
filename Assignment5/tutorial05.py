b2c675f76836f9f52cb4f63e426e1741aea9bc89
Pritam Singh <pkspritam10@gmail.com>
2020-11-13 14:24:07 +0530

    implemented rename_Suits function;to rename Suits
    Signed-off-by: Pritam Singh <pkspritam10@gmail.com>

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -123,6 +123,30 @@ def rename_Sherlock(folder_name):
 
 def rename_Suits(folder_name):
     # rename Logic 
+    print("Enter Main Title of the Web Series :")
+    series_name = input()
+    print("Enter Season Number Padding :")
+    season_padding = '0{}'.format(input())
+    print("Enter Episode Number Padding :")
+    episode_padding = '0{}'.format(input())
+    for file_name in os.listdir(folder_name):
+        season = -1
+        episode = -1
+        episode_name = -1
+        if len(re.findall(r'- (\d+)x(\d+) -( (.*?)\.)',file_name)): # Suits - 5x04 - No Puedo Hacerlo.HDTV.ASAP.en.mp4
+            temp = re.findall(r'- (\d+)x(\d+) -( (.*?)\.)',file_name)[0]
+            season = temp[0]
+            episode = temp[1]
+            episode_name = temp[-1]
+        name = '{} {} {} {}'.format(series_name,format(int(season),season_padding),format(int(episode),episode_padding),episode_name)
+        if re.search(r'mp4$',file_name):
+            name = '{}.mp4'.format(name)
+        elif re.search(r'srt$',file_name):
+            name = '{}.srt'.format(name)
+            pass
+        else:
+            pass
+        os.rename('{}/{}'.format(folder_name,file_name),'{}/{}'.format(folder_name,name))
     pass
     
 
