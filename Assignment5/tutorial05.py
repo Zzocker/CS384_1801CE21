2a434c66a034d5690d66cc1278138b57311078bb
Pritam Singh <pkspritam10@gmail.com>
2020-11-13 14:03:15 +0530

    implemented rename_Sherlock function;to rename Sherlock
    Signed-off-by: Pritam Singh <pkspritam10@gmail.com>

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -86,6 +86,38 @@ def rename_Game_of_Thrones(folder_name):
 
 def rename_Sherlock(folder_name):
     # rename Logic 
+    print("Enter Main Title of the Web Series :")
+    series_name = input()
+    print("Enter Season Number Padding :")
+    season_padding = '0{}'.format(input())
+    print("Enter Episode Number Padding :")
+    episode_padding = '0{}'.format(input())
+    for file_name in os.listdir(folder_name):
+        season = -1
+        episode = -1
+        episode_name = -1
+        if len(re.findall(r'\.S(\d+)\.E(\d+)',file_name)) == 1: # Sherlock.S01.E01.480p.srt
+            temp = re.findall(r'\.S(\d+)\.E(\d+)',file_name)[0]
+            season=temp[0]
+            episode=temp[1]
+        if len(re.findall(r'\.S(\d+)E(\d+)',file_name)) == 1: # Sherlock.S04E03.720p.x265.mp4
+            temp = re.findall(r'\.S(\d+)E(\d+)',file_name)[0]
+            season=temp[0]
+            episode=temp[1]
+        name = ''
+        if episode_name != -1:
+            name = '{} {} {} {}'.format(series_name,format(int(season),season_padding),format(int(episode),episode_padding),episode_name)
+        else:
+            name = '{} {} {}'.format(series_name,format(int(season),season_padding),format(int(episode),episode_padding))
+        if re.search(r'mp4$',file_name):
+            name = '{}.mp4'.format(name)
+        elif re.search(r'srt$',file_name):
+            name = '{}.srt'.format(name)
+            pass
+        else:
+            pass
+        os.rename('{}/{}'.format(folder_name,file_name),'{}/{}'.format(folder_name,name))
+
     pass
     
 
